9851d7785af0f8e7fcc0fce66eb91581b37c1501
CCM7337 <covenantconnectionministry7337@gmail.com>
2025-05-05 14:03:10 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// Main App structure
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+export default function App() {
+ return (
+  <Router>
+   <Routes>
+    <Route path='/' element={<Home />} />
+   </Routes>
+  </Router>
+ );
+}
